1764fa4692399d3b24487f812549cd9957d1d7dd
Sergio Cano <canosergio90@gmail.com>
2022-08-23 09:26:39 +0200

    setting up next ui
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Home Page</h1>
+      <Button color='gradient'>Click me!</Button>
     </>
   )
 }
