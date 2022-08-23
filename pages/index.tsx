ab6337360a037035031c043618f1c39af263e04d
Sergio Cano <canosergio90@gmail.com>
2022-08-23 23:29:13 +0200

    retrieving static props from pokemon api
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 import { Layout } from '../components/layouts'
+import { GetStaticProps } from 'next'
+import { pokeApi } from '../api'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+
+  console.log(props);
+  
+
   return (
     <>
       <Layout title='Listado de Pokemons'>
@@ -11,6 +17,18 @@ const Home: NextPage = () => {
       </Layout>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get('/pokemon?limit=151')
+
+
+
+  return {
+    props: {
+      pokemons: data.results // TODO add types
+    }
+  }
 }
 
 export default Home
